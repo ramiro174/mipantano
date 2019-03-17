ebe8fbf80650afe15a8d54310a286ef3eed014d7
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-03-17 12:13:11 -0600

    ad ruta api

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/login', function (Request $request) {
-    return $request->user();
+    return "aaa";
 });
