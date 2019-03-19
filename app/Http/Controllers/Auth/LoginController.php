cc38e7c54df95020ab21eb8bc4955e17a0697b95
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-03-19 00:28:48 -0600

    ad ruta api

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -53,6 +54,7 @@ class LoginController extends Controller
         }
         
         if ($this->attemptLogin($request)) {
+            
             return  ["user"=>Auth::user(),"api_token"=>Auth::user()->api_token];
         }
         
