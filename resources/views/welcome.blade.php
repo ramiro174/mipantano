90a36087c4c1a849fc41277d3f498b7cbd4d4514
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-03-19 00:13:44 -0600

    ad ruta api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ramiro
                 </div>
 
                 <div class="links">
