0b2fbcfac55197b4c534b045d782d3947c268a26
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-03-19 00:11:47 -0600

    ad ruta api

--- routes/api.php
+++ routes/api.php
@@ -3,28 +3,23 @@
     use App\Http\Controllers\Auth\LoginController;
     use App\User;
     use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::post('/login', function (Request $request) {
-     $lg=new LoginController();
-     
-     dd($request);
     
-    return $usuario=$lg->loginapi($request);
-    
-    
-});
+    /*
+    |--------------------------------------------------------------------------
+    | API Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register API routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | is assigned the "api" middleware group. Enjoy building your API!
+    |
+    */
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/login', function (Request $request) {
+        $lg = new LoginController();
+        dd($request);
+        return $usuario = $lg->loginapi($request);
+    });
 
